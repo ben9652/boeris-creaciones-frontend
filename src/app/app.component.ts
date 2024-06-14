cd9f1401d953f307268f30791d0d535761e5e7ba
Benjamín Boeri <ben9652@gmail.com>
2024-06-13 23:03:37 -0300

    Añado las librerías PrimeNG, PrimeIcons, y FontAwesome. PrimeIcons servirán para iconos que acompañen exclusivamente a los botones de PrimeNG y FontAwesome para lo demás.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { CustomTranslateService } from './services/custom-translate.service';
+import { ButtonModule } from 'primeng/button';
+import { FontAwesomeModule, IconDefinition } from '@fortawesome/angular-fontawesome';
+
+// En esta versión se importarán los iconos que necesite cada componente. De esta manera evitamos una sobrecarga innecesaria.
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TranslateModule,
+    ButtonModule,
+    FontAwesomeModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'boeris-creaciones-frontend';
+  faCoffee: IconDefinition = faCoffee
+
+  constructor(
+    private translateService: CustomTranslateService
+  ) {
+
+  }
+
+  // Uso de NgxTranslate en archivo TS
+  async ngOnInit(): Promise<void> {
+    let translatedText: string | undefined;
+    translatedText = await this.translateService.getTranslation('TITLE');
+    console.log('Título pero con Translate: ', translatedText);
+  }
 }
--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,27 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { mergeApplicationConfig, ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient, '/i18n/', '.json');
+}
+
+const translateProviders = TranslateModule.forRoot({
+  loader: {
+    provide: TranslateLoader,
+    useFactory: HttpLoaderFactory,
+    deps: [HttpClient]
+  }
+})
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideHttpClient(withFetch()),
+    importProvidersFrom(translateProviders),
+    provideServerRendering(),
   ]
 };
 
