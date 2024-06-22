632ea0c9e32a927c05b96ac804af2f6f092d9010
Benjamín Boeri <ben9652@gmail.com>
2024-06-22 19:02:35 -0300

    Configuro correctamente el servicio de traducción | Agrego un control a la existencia del archivo .env | Agrego el icono del proyecto | Hago uso de la librería de iconos de FontAwesome de tal manera que solo con la clase puedo referenciar a un icono | Agrego los estilos faltantes de PrimeNG con el archivo primeng.min.css | Elimino los archivo innecesarios para la configuración de los estilos personalizados de PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TranslateModule } from '@ngx-translate/core';
-import { CustomTranslateService } from './services/custom-translate.service';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { ButtonModule } from 'primeng/button';
-import { FontAwesomeModule, IconDefinition } from '@fortawesome/angular-fontawesome';
 import { environment } from '../environments/environment'
-
-// En esta versión se importarán los iconos que necesite cada componente. De esta manera evitamos una sobrecarga innecesaria.
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -15,27 +10,25 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
   imports: [
     RouterOutlet,
     TranslateModule,
-    ButtonModule,
-    FontAwesomeModule
+    ButtonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'boeris-creaciones-frontend';
-  faCoffee: IconDefinition = faCoffee;
   apiUrl?: string; // Ejemplo de uso de una variable de entorno
+  translatedTitle?: string;
 
   constructor(
-    private translateService: CustomTranslateService
+    public translateService: TranslateService
   ) {
     this.apiUrl = environment.API_URL; // Ejemplo de uso de variable de entorno
+    this.translatedTitle = translateService.instant('TITLE');
   }
 
   // Uso de NgxTranslate en archivo TS
-  async ngOnInit(): Promise<void> {
-    let translatedText: string | undefined;
-    translatedText = await this.translateService.getTranslation('TITLE');
-    console.log('Título pero con Translate: ', translatedText);
+  ngOnInit() {
+    
   }
 }
