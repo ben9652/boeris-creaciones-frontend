0a7b21e61858cd2313aad51be0d7b6732f0f5a3d
Benjamín Boeri <ben9652@gmail.com>
2024-06-27 22:02:13 -0300

    Corrección: olvidé proveer el interceptor en la configuración global

--- src/app/core/interceptors/requests-response.interceptor.ts
+++ src/app/core/interceptors/requests-response.interceptor.ts
@@ -4,16 +4,6 @@ import { MessageService } from "primeng/api";
 import { catchError, throwError } from "rxjs";
 
 function handleErrorResponse(error: HttpErrorResponse)  {
-    // const messageService = inject(MessageService);
-    // if(error.status === 403) {
-    //     messageService.add({
-    //         severity: 'error',
-    //         // TODO: Agregar al translate estos mensajes y hacer uso de ellos
-    //         summary: 'Denegado',
-    //         detail: 'No estás autorizado para realizar esta acción'
-    //     });
-    // }
-    // const errorResponse = `Error`
     return throwError(() => {
         switch(error.status) {
             case 403:
