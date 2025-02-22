f3ed8ab698b15ce02886f8bb2d497fd5e454e0a5
nachoalgz <nacho.algz96@gmail.com>
2025-02-22 20:43:08 -0300

    Cambio el valor de las variables en discardObject, elimino los setters y corrijo los getters. Saco el atributo [max] del inputnumber de cantidad.

--- src/app/core/models/discardObject.entities.ts
+++ src/app/core/models/discardObject.entities.ts
@@ -1,27 +1,19 @@
 export class DiscardObject {
-    private _reason: string;
-    private _quantity: number;
+    private reason: string;
+    private quantity: number;
 
     constructor(reason: string, quantity: number){
-        this._reason = reason;
-        this._quantity = quantity;
+        this.reason = reason;
+        this.quantity = quantity;
     }
 
     
-    public get reason() : string {
-        return this._reason
+    public getReason() : string {
+        return this.reason;
     }
-    
-    public set reason(value : string) {
-        this._reason = value;
-    }
-    
-    public get quantity() : number {
-        return this._quantity
-    }
-    
-    public set quantity(value : number) {
-        this._quantity = value;
+
+    public getQuantity() : number {
+        return this.quantity;
     }
     
 }
